f1bc91c988e0c87415648541d6b897179fec4a08
Rayhan Hossen <rayhanhossen.cse@gmail.com>
2023-03-31 01:26:41 +0600

    Some changes and added

--- module/api/sms_api.py
+++ module/api/sms_api.py
@@ -1,8 +1,31 @@
+import ssl
+import urllib
+
+from logger import Logger
+
+from module.config.config import ConfigParser
+
+# instance of other class
+logger = Logger.get_instance()
+config = ConfigParser().get_config()
+
+
 class SmsAPI:
-    def __init__(self, sms_api_url, username, password):
-        self.sms_api_url = sms_api_url
-        self.username = username
-        self.password = password
+    def __init__(self):
+        self.sms_api_url = config.sms_api_url
+        self.username = config.sms_api_username
+        self.password = config.sms_api_password
 
     def send(self, msisdn, msg):
-        pass
+        try:
+            ssl._create_default_https_context = ssl._create_unverified_context
+            msg = urllib.parse.quote_plus(msg, encoding='UTF-8')
+            final_url = f"{self.sms_api_url}?user=CallDropMin&password=C@11Dr0pM!n&src=Robi%20Rebate&dst={msisdn}&msg={msg}&dr=1&type=u"
+            resource = urllib.request.urlopen(final_url)
+            response = resource.read()
+            response = str(response)
+            response = response.strip("b'")
+            print(f'Response - {response}')
+            logger.log_info(f'Response - {response}')
+        except Exception as e:
+            logger.log_error(exception=e)
--- module/config/config.py
+++ module/config/config.py
@@ -1,6 +1,5 @@
 import json
 import sys
-import pandas as pd
 
 from module.encryption.crypto_pass_phase import CryptoPassPhase
 
@@ -22,88 +21,6 @@ class ConfigParser:
         encrypted = CryptoPassPhase.encrypt(pass_phase, key)
         print(encrypted)
         self.stop_execution()
-
-    def get_credentials(self):
-        link = self.conf['crm_link']
-        pass_phase = self.conf['PassPhase']
-        interaction_type = self.conf['interaction_type']
-        ins_product = self.conf['ins_product']
-        ins_area = self.conf['ins_area']
-        ins_sub_area = self.conf['ins_sub_area']
-        ins_status = self.conf['ins_status']
-        crm_username = self.conf['crm_username']
-        crm_password = CryptoPassPhase.decrypt(pass_phase, self.conf['crm_password'])
-        credentials = pd.DataFrame(
-            columns=[
-                "link",
-                "username",
-                "password",
-                "interaction_type",
-                "ins_product",
-                "ins_area",
-                "ins_sub_area",
-                "ins_status"
-            ],
-            data=[
-                [
-                    link,
-                    crm_username,
-                    crm_password,
-                    interaction_type,
-                    ins_product,
-                    ins_area,
-                    ins_sub_area,
-                    ins_status
-                ]
-            ]
-        )
-        return credentials
-
-    def set_workflow_config(self, app_id, del_index=0, rpa_status='', remarks=''):
-        credentials = pd.DataFrame(
-            columns=[
-                "username",
-                "password",
-                "app_id",
-                "del_index",
-                "rpa_status",
-                "remarks",
-            ],
-            data=[
-                [
-                    "finance_rpa",
-                    "Pass*12345",
-                    app_id,
-                    del_index,
-                    rpa_status,
-                    remarks,
-                ]
-            ]
-        )
-        return credentials
-
-    def get_reporting_email(self):
-        pass
-
-    def get_error_reporting_email(self):
-        to = self.conf['error_to']
-        return to.split(',')
-
-    def get_call_drop_report_email_to(self):
-        to = self.conf['call_drop_email_to']
-        return to.split(',')
-
-    def get_call_drop_sms_to_concern(self):
-        to = self.conf['call_drop_sms_to_concern']
-        return to.split(',')
-
-    def get_sms_sucsess_message(self):
-        to = self.conf['success_sms']
-        return to
-
-    def get_sms_error_message(self):
-        to = self.conf['not_success_sms']
-        return to
 
     def get_config(self):
         return self.conf
